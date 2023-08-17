0e01db1aa7174c75a05245cdcc5556e1661b2044
Patrick Sandin <patrick@sandin.tech>
2023-08-17 16:19:24 -0700

    Solves fizzbuzz in O(n) time and space complexity.

--- fizz_buzz/solution.py
+++ fizz_buzz/solution.py
@@ -3,7 +3,7 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         result = []
         for integer in range(1, n + 1):
-            if integer % 3 == 0 and integer % 5 == 0:
+            if integer % 15 == 0:
                 result.append("FizzBuzz")
             elif integer % 3 == 0:
                 result.append("Fizz")
