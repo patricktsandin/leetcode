2bd87fd1a1095a0b631f407fa6617c001b190b2f
Patrick Sandin <patrick@sandin.tech>
2023-08-27 14:06:12 -0700

    Merges two sorted lists in-place in O(n * log n) time and O(1) space.

--- merge_sorted_array/solution.py
+++ merge_sorted_array/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     """Merges two sorted lists in-place in O(n * log n) time and O(1) space."""
     def merge(self, nums1, m, nums2, n):
-        if n:
-            del nums1[-n:]
+        del nums1[m:]
         nums1.extend(nums2)
         nums1.sort()
