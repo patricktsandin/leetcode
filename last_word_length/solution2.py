0b56bf03d8c63c3abaccb22881b87c48ae6e2dbc
Patrick Sandin <patrick@sandin.tech>
2023-08-28 16:03:38 -0700

    Get length of last space-delimited element from string in O(n) time and space.

---
+++ last_word_length/solution2.py
@@ -0,0 +1,5 @@
+class Solution:
+    """Get length of last space-delimited element from string in
+    O(n) time and space."""
+    def lengthOfLastWord(self, s):
+        return len(s.rsplit(maxsplit=1)[-1])
