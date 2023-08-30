3c5d868560169d24e9784dee5c4adf22416cb936
Patrick Sandin <patrick@sandin.tech>
2023-08-29 20:50:18 -0700

    Return whether two provided binary trees are the same in O(1) space and O(n) time.

---
+++ same_tree/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSameTree(self, p, q):
+        """Return whether two provided binary trees are the same in
+        O(1) space and O(n) time."""
+        return (
+            not (p or q)
+            or (p and q)
+            and p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )
