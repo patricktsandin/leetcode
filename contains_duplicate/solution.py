c8f8b4b4fe1814cc173223fe2afa8a493b6caa98
Patrick Sandin <patrick@sandin.tech>
2023-08-31 20:49:02 -0700

    Comment tweak.

--- contains_duplicate/solution.py
+++ contains_duplicate/solution.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 
 class Solution:
-    """Detects if string of ints contains duplicates in O(n) time and
+    """Detects if a string of integers contains duplicates in O(n) time and
     O(n) space."""
     def containsDuplicate(self, nums):
         return max(Counter(nums).values()) > 1
