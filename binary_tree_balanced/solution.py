eec1121e87b970100ab405f25911ee4623316ef7
Patrick Sandin <patrick@sandin.tech>
2023-09-01 21:32:39 -0700

    Return whether child trees are height-balanced and maximum depth in O(n) time and O(log n) space.

--- binary_tree_balanced/solution.py
+++ binary_tree_balanced/solution.py
@@ -1,10 +1,3 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
     def is_balanced(self, root):
         """Return whether child trees are height-balanced and maximum depth
