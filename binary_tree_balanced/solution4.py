1124f820268b6a4dc411103d3d25ea4d3310a959
Patrick Sandin <patrick@sandin.tech>
2023-09-03 09:34:45 -0700

    Returns whether a binary tree is height-balanced in O(n) time and O(log n) space using an iterative algorithm.

--- binary_tree_balanced/solution4.py
+++ binary_tree_balanced/solution4.py
@@ -2,59 +2,23 @@ from operator import sub
 from functools import reduce
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
+    """Returns whether a binary tree is height-balanced in O(n) time and
+    O(log n) space."""
     def isBalanced(self, root):
         balanced = True
-        if root:
-            stack: list = [root]
-            root.depths = []
-            root.prev = None
-            while stack and balanced:
-                node = stack[-1]
-                children = list(filter(None, [node.left, node.right]))
-                if children:
-                    for child in children:
-                        child.prev = node
-                        child.depths = []
-                    stack.extend(children)
-                    continue
-                else:
-                    stack.pop()
-                    if len(node.depths) == 2:
-                        breakpoint()
-                        balanced = abs(
-                            reduce(sub, node.depths)
-                        ) <= 1 and balanced
-                    if node.depths and node.prev:
-                        node.prev.depths.append(max(node.depths) + 1)
-                    if node.prev and node is node.prev.right:
-                        node.prev.right = None
-                    if node.prev and node is node.prev.left:
-                        node.prev.left = None
+        if root: root.val, root.prev = [], None
+        while root and balanced:
+            children = list(filter(None, [root.left, root.right]))
+            if children:
+                for child in children: child.val, child.prev = [], root
+                root = children[-1]
+            else:
+                if not root.val: root.val = [0]
+                balanced = abs(reduce(sub, root.val)) <= 1 and balanced
+                if root.prev:
+                    root.prev.val.append(max(root.val) + 1)
+                    if root is root.prev.right: root.prev.right = None
+                    if root is root.prev.left: root.prev.left = None
+                root = root.prev
         return balanced
-
-
-tree = TreeNode(
-    right=TreeNode(
-        right=TreeNode(),
-        left=TreeNode(
-            right=TreeNode(),
-            left=TreeNode(
-                right=TreeNode(),
-                left=TreeNode()
-            )
-        )
-    ),
-    left=TreeNode()
-)
-
-print(
-    Solution().isBalanced(tree)
-)
