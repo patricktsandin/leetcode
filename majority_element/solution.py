638508a9c49f663f0cdb3d3ac43741bb82d61f2d
Patrick Sandin <patrick@sandin.tech>
2023-09-04 14:08:08 -0700

    Returns the majority element from an array in O(n) time and O(1) space.

--- majority_element/solution.py
+++ majority_element/solution.py
@@ -3,6 +3,6 @@ from collections import Counter
 
 class Solution:
     """Returns the majority element from an array in O(n) time
-    and O(n) space."""
+    and O(1) space."""
     def majorityElement(self, nums):
         return Counter(nums).most_common(1)[0][0]
