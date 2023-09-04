44807d7742b4ccffc0b1ed7ec382051e9e571967
Patrick Sandin <patrick@sandin.tech>
2023-09-04 13:25:32 -0700

    Returns whether a linked list has a cycle in O(n) time and O(1) space.

--- linked_list_cycle/solution2.py
+++ linked_list_cycle/solution2.py
@@ -2,9 +2,10 @@ class Solution:
     def hasCycle(self, head):
         """Returns whether a linked list has a cycle in O(n) time and O(1)
         space."""
-        while head and head.next:
-            if head.val is None:
+        while head and head.next is not None:
+            if head.next is False:
                 return True
-            head.val = None
-            head = head.next
+            next = head.next
+            head.next = False
+            head = next
         return False
