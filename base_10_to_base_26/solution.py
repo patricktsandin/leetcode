b1e721097ecdadc03a9019ae709caa5c36ff813c
Patrick Sandin <patrick@sandin.tech>
2023-09-07 19:31:05 -0700

    Converts base 26 alphabetic string representation of integer to base 10 representation in linear time and space using iteration.

--- base_10_to_base_26/solution.py
+++ base_10_to_base_26/solution.py
@@ -1,4 +1,8 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        """Returns base 26 alphabetic string representation of integer."""
-        pass  # Work in progress
+        """Converts base 26 alphabetic string representation of integer
+        to base 10 representation in linear time and space using iteration."""
+        result = 0
+        for index, char in enumerate(reversed(columnTitle)):
+            result += (ord(char) - 64)*26**index
+        return result
