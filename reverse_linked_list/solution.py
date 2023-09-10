c49118c8e72538d1617b2659c50dde16bc2b178e
Patrick Sandin <patrick@sandin.tech>
2023-09-10 12:40:29 -0700

    Adds solution to reverse a linked list.  Iterative algorithm, constant space and linear time.

---
+++ reverse_linked_list/solution.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    @staticmethod
+    def reverse_list(node1: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Reverses a linked list in linear time and constant space.
+        :param node1: Head of a linked list
+        :return: Head of reversed linked list
+        """
+        if node1:
+            node2, node1.next = node1.next, None
+            while node2:
+                node1, node2.next, node2 = node2, node1, node2.next
+        return node1
