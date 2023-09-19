cc2c23219d0b9e83b51d0cad9eba0fd26dbbfdc9
Patrick Sandin <patrick@sandin.tech>
2023-09-18 20:38:25 -0700

    Adds WIP solution to remove matching linked list elements.

---
+++ easy/python/remove_linked_list_elements/solution.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def remove_elements(
+        self, head: Optional[ListNode], val: int
+    ) -> Optional[ListNode]:
+        # Work in progress
+        pass
