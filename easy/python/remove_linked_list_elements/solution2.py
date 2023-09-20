2a6fd29218a9cc6ba6623a1d0d7ba0e2ba687789
Patrick Sandin <patrick@sandin.tech>
2023-09-19 19:59:54 -0700

    Adds solution to remove matching linked list elements.

--- easy/python/remove_linked_list_elements/solution2.py
+++ easy/python/remove_linked_list_elements/solution2.py
@@ -13,19 +13,16 @@ class Solution:
     ) -> Optional[ListNode]:
         """
         Return head of a linked list with all nodes matching val removed.
-        Iterative algorithm in linear time and constant space.
-        Beats 77.74% of LeetCoders on memory consumption.
+        Recursive algorithm in linear time and space.
+        Beats 91% of LeetCoders on compute.
         :param head: Head of linked list possibly containing 'val'
         :param val: Value of nodes to remove
         :return: Head of linked list without nodes of value 'val'
         """
-        pointer = head
-        while pointer:
-            if pointer is head and pointer.val == val:
-                head = head.next
-                pointer.next, pointer = None, pointer.next
-            elif pointer.next and pointer.next.val == val:
-                pointer.next = pointer.next.next
+        if head:
+            child = self.remove_elements(head.next, val)
+            if head.val == val:
+                head = child
             else:
-                pointer = pointer.next
+                head.next = child
         return head
