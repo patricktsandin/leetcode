d05dbe15f9d3c22767dfe9af617426b7c1a10049
Patrick Sandin <patrick@sandin.tech>
2023-09-20 17:50:37 -0700

    Adds solution to detect whether list contains duplicates within a sliding window of arbitrary size in multilinear time and linear space.

---
+++ easy/python/contains_duplicate_ii/solution.py
@@ -0,0 +1,25 @@
+# Given an integer array nums and an integer k, return true if there are two
+# distinct indices i and j in the array such that nums[i] == nums[j] and abs(
+# i - j) <= k.
+#
+# Constraints:
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 0 <= k <= 105
+
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def contains_nearby_duplicate(nums: List[int], k: int) -> bool:
+        """
+        Returns whether list nums contains any duplicates within a sliding
+        window of length k+1 using an iterative algorithm in
+        subquadratic/multilinear O(len(nums)*k) time and linear O(k) space.
+        """
+        for index in range(len(nums)):
+            window = nums[index:index + k + 1]
+            if len(set(window)) < len(window):
+                return True
+        return False
