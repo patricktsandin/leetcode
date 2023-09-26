822d0a62b6a7f7da0cd81cea505728984e22d01d
Patrick Sandin <patrick@sandin.tech>
2023-09-25 20:53:06 -0700

    Adds solution to return whether an integer is a power of two.

---
+++ easy/python/power_of_two/solution2.py
@@ -0,0 +1,8 @@
+# Given an integer n, return true if it is a power of two. Otherwise, return
+# false.
+
+class Solution:
+    """Returns whether an integer is a power of two"""
+    @staticmethod
+    def is_power_of_two(n: int) -> bool:
+        return n > 0 and n & (n-1) == 0
