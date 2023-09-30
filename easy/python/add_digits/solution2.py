74fb036f65a8dd9c10c584f9c7c105734e06fb32
Patrick Sandin <patrick@sandin.tech>
2023-09-29 17:14:50 -0700

    Adds solution to get the intersection of two sets.

--- easy/python/add_digits/solution2.py
+++ easy/python/add_digits/solution2.py
@@ -1,9 +1,12 @@
-# Given an integer num, repeatedly add all its digits until the result has only
-# one digit, and return it.
+# Given two integer arrays nums1 and nums2, return an array of their
+# intersection. Each element in the result must be unique and you may return
+# the result in any order.
+
+from typing import List, Set
+
 
 class Solution:
     @staticmethod
-    def add_digits(number: int) -> int:
-        """Adds the digits of provided number and all results
-        until answer is a single digit."""
-        return ((number - 1) % 9 + 1) if number else number
+    def intersection(nums1: List[int], nums2: List[int]) -> Set[int]:
+        """Returns intersection of two arrays."""
+        return set(nums1) & set(nums2)
