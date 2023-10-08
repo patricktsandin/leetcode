cc2a49136eb60410992d1975a027bd7a6622cf28
Patrick Sandin <patrick@sandin.tech>
2023-10-07 18:57:58 -0700

    Adds WIP solution to return number of square triples in a range.

---
+++ easy/python/count_square_sum_triples/solution.py
@@ -0,0 +1,11 @@
+# A square triple (a,b,c) is a triple where a, b, and c are integers and a2 +
+# b2 = c2.
+#
+# Given an integer n, return the number of square triples such that 1 <= a,
+# b, c <= n.
+
+class Solution:
+    @staticmethod
+    def count_triples(self, n: int) -> int:
+        # WIP
+        pass
