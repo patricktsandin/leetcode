1485053c070d964a4efb75380f314ca767f2c4d1
Patrick Sandin <patrick@sandin.tech>
2023-10-08 17:56:18 -0700

    Adds WIP solution to return number of square triples in a range.

--- easy/python/count_square_sum_triples/solution.py
+++ easy/python/count_square_sum_triples/solution.py
@@ -4,8 +4,17 @@
 # Given an integer n, return the number of square triples such that 1 <= a,
 # b, c <= n.
 
+from math import sqrt
+
+
 class Solution:
     @staticmethod
-    def count_triples(self, n: int) -> int:
-        # WIP
-        pass
+    def count_triples(n: int) -> int:
+        triples = 0
+        while n > 4:
+            if n % 2 != 0 and sqrt(n**2 - (n - 1)**2).is_integer():
+                triples += 2
+            if n > 5 and sqrt(n**2 - (n - 2)**2).is_integer():
+                triples += 2
+            n -= 1
+        return triples
