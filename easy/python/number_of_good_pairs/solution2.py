1e79f6530fe1355c3c1b80501a17ecb0d4882433
Patrick Sandin <patrick@sandin.tech>
2023-10-12 17:54:22 -0700

    Adds solution to count number of 'good pairs' in an array.

---
+++ easy/python/number_of_good_pairs/solution2.py
@@ -0,0 +1,14 @@
+# Given an array of integers nums, return the number of good pairs.
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    """Solves using iterative algorithm in linear time and space."""
+
+    @staticmethod
+    def num_identical_pairs(nums: List[int]) -> int:
+        """Count number of 'good pairs' in a list."""
+        return sum(map(lambda n: (n*(n - 1))//2, Counter(nums).values()))
