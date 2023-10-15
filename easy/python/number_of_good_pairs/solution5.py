b49d484133adb948f8a3e86047a162bf2abb95a8
Patrick Sandin <patrick@sandin.tech>
2023-10-14 20:52:03 -0700

    Updates solution to count number of 'good pairs' in an array.

--- easy/python/number_of_good_pairs/solution5.py
+++ easy/python/number_of_good_pairs/solution5.py
@@ -1,5 +1,7 @@
-# Given an array of integers nums, return the number of good pairs.
-# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+"""
+Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+"""
 
 from typing import List
 from collections import Counter
