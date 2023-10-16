b363b1f8beb0e88439f79bfb607bdb806420d664
Patrick Sandin <patrick@sandin.tech>
2023-10-16 15:56:02 -0700

    Adds solution to apply successive operations to variable initialized to 0.

---
+++ easy/python/final_value_after_operations/solution6.py
@@ -0,0 +1,27 @@
+"""
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return
+the final value of X after performing all the operations.
+"""
+
+from typing import List
+
+
+class Solution:
+    """Solves with iterative algorithm in linear time and constant space."""
+
+    @staticmethod
+    def final_value_after_operations(operations: List[str]) -> int:
+        """Applies operations to variable initialized to 0"""
+        x = 0
+        for operation in operations:
+            if operation == "++X" or operation == "X++":
+                x += 1
+            else:
+                x -= 1
+        return x
