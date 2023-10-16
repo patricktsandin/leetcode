807e970a19f6169228123c078a24a616ddac6c39
Patrick Sandin <patrick@sandin.tech>
2023-10-15 17:38:22 -0700

    Adds solution to 'defang' an IP address.

---
+++ easy/python/defanging_an_ip_address/solution2.py
@@ -0,0 +1,14 @@
+"""
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+A defanged IP address replaces every period '.' with '[.]'.
+"""
+
+
+class Solution:
+    """Solves in linear time and space using an iterative algorithm"""
+
+    @staticmethod
+    def defang_ip_address(address: str) -> str:
+        """Return an IPv4 address, surrounding all separators
+        with brackets."""
+        return address.replace(".", "[.]")
