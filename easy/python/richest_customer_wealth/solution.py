33c4a05db18717e6a8efe44e768eaa6b89d36c44
Patrick Sandin <patrick@sandin.tech>
2023-10-17 19:57:19 -0700

    Updates solution to use generator expression for space efficiency.

--- easy/python/richest_customer_wealth/solution.py
+++ easy/python/richest_customer_wealth/solution.py
@@ -1,4 +1,19 @@
+"""
+You are given an m x n integer grid accounts where accounts[i][j] is the
+amount of money the ith customer has in the jth
+bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank
+accounts. The richest customer is the customer that has the maximum wealth.
+"""
+
+from typing import List
+
+
 class Solution:
-    """Return sum of largest row in matrix"""
-    def maximumWealth(self, accounts):
-        return max([sum(account) for account in accounts])
+    """Solves with iterative algorithm in linear time and constant space."""
+
+    @staticmethod
+    def maximum_wealth(accounts: List[List[int]]) -> int:
+        """Return sum of largest row in matrix."""
+        return max(sum(account) for account in accounts)
