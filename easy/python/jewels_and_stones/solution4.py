650228c6f0e5346ca437207eabad2f675f8e98aa
Patrick Sandin <patrick@sandin.tech>
2023-10-22 16:43:41 -0700

    Adds solution to count matching characters.

---
+++ easy/python/jewels_and_stones/solution4.py
@@ -0,0 +1,23 @@
+"""
+You're given strings jewels representing the types of stones that are jewels,
+and stones representing the stones you have. Each character in stones is a
+type of stone you have. You want to know how many of the stones you have are
+also jewels.
+
+Letters are case sensitive, so 'a' is considered a different type of stone
+from 'A'.
+"""
+
+
+class Solution:
+    """Solves in linear time and constant space."""
+
+    @staticmethod
+    def num_jewels_in_stones(jewels: str, stones: str) -> int:
+        """
+        Return count of characters in 'stones' matching those
+        in 'jewels'
+        :param jewels: Characters to match against
+        :param stones: Characters to count
+        """
+        return sum(1 for stone in stones if stone in set(jewels))
